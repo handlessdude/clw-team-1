cba72420774c5c5520a4010dd1d18d7263779224
Egor <brodiaga89@gmail.com>
2021-10-27 00:46:41 +0300

    Set DelTrack method&

--- src/components/track-item.vue
+++ src/components/track-item.vue
@@ -1,12 +1,13 @@
 <template>
   <div class="track-item" @click="$router.push(`/catalogue/${track.id}`)">
     <div class="track-info">
-<!--      <div><strong>ID трека:</strong> {{ track.id }}</div>-->
-<!--      <div><strong>Название:</strong> {{ track.data.name}}</div>-->
-      <h3>{{ track.data.name}}</h3>
-<!--      <div><strong>Описание:</strong> {{ track.data.previewText }}</div>-->
+      <!--      <div><strong>ID трека:</strong> {{ track.id }}</div>-->
+      <!--      <div><strong>Название:</strong> {{ track.data.name}}</div>-->
+      <h3>{{ track.data.name }}</h3>
+      <button @click.stop="deleteTrack(track)">Delete</button>
+      <!--      <div><strong>Описание:</strong> {{ track.data.previewText }}</div>-->
     </div>
-<!--    <div class="track-item__btns">
+    <!--    <div class="track-item__btns">
       <my-button
           @click.stop="$emit('remove', track)"
       >
@@ -16,19 +17,27 @@
 </template>
 
 <script>
+import TrackApi from "../api/Track";
+import { mapMutations } from "vuex";
 
 export default {
   name: "track-item",
-  components: {
-
-  },
-  props:{
+  components: {},
+  props: {
     track: {
       type: Object, //type of out prop 'track'
       required: true, //info about track is sufficient
-    }
-  }
-}
+    },
+  },
+  methods: {
+    ...mapMutations(["deleteTrack"]),
+
+    deleteTrack(track) {
+      TrackApi.delete(track);
+      this.$store.commit("trackCatalogue/deleteTrack", track.id);
+    },
+  },
+};
 </script>
 
 <style scoped>
@@ -39,17 +48,16 @@ export default {
   align-items: center;
   justify-content: space-between;
   border-radius: 12px;
-
 }
-.track-item:hover{
+.track-item:hover {
   cursor: pointer;
 }
-.track-info{
+.track-info {
   display: flex;
   flex-direction: column;
 }
-.track-info *{
-  text-align:left;
+.track-info * {
+  text-align: left;
 }
 .track-item__btns {
   display: flex;
