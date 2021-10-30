1103a53a24d9a84d5baeaf04b82c587c59c2136e
Egor Matiushkin <brodiaga89@gmail.com>
2021-10-30 21:00:29 +0400

    finishPage

--- src/App.vue
+++ src/App.vue
@@ -7,41 +7,39 @@
 
 <script>
 import Sidebar from "./components/sidebar";
-import {mapGetters} from "vuex";
+import { mapGetters } from "vuex";
 export default {
-  name: 'App',
+  name: "App",
   components: {
-    Sidebar
+    Sidebar,
   },
   computed: {
     ...mapGetters({
-      getSidebarWidth: 'sidebar/getSidebarWidth',
-    })
+      getSidebarWidth: "sidebar/getSidebarWidth",
+    }),
   },
-}
+};
 </script>
 
 <style lang="scss">
-@import url('https://fonts.googleapis.com/css?family=Roboto&display=swap');
+@import url("https://fonts.googleapis.com/css?family=Roboto&display=swap");
 
-*{
+* {
   margin: 0;
   padding: 0;
   box-sizing: border-box;
 }
 #app {
   /*font-family: Avenir, Helvetica, Arial, sans-serif;*/
-  font-family: 'Roboto', sans-serif;
+  font-family: "Roboto", sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   /*color: #122523;
   color: #5daba4;*/
   color: #343434;
-  padding: 20px;
   height: 100vh;
   /*
-  width: 100vw;
   background: linear-gradient(220.16deg, #25414d -6%, #380c49  138%);*/
   background: #f2f2f2;
 }
--- src/pages/Main.vue
+++ src/pages/Main.vue
@@ -1,15 +1,255 @@
 <template>
-<div>
-  <h1>Добро пожаловать!</h1>
-</div>
+  <div class="mainpage">
+    <div class="mainpage__header">
+      <div class="mainpage__header_welcome">
+        <h3 class="mainpage__header_headtext">
+          Добро пожаловать {{ userName }}
+        </h3>
+        <p class="mainpage__header_maintext">
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc et quam
+          quis dui imperdiet varius nec et magna. Phasellus facilisis libero
+          erat, in elementum lectus posuere sed.
+        </p>
+      </div>
+      <div class="mainpage__header_user">
+        <div class="mainpage__header_userinfo">
+          <img
+            class="mainpage__header_useravatar"
+            src="../assets/useravatar.png"
+            alt="User Avatar"
+          />
+          <div class="mainpage__header_userparams">
+            <h3 class="mainpage__header_userparamsname">{{ userName }}</h3>
+            <p class="mainpage__header_userparamsstatus">Статус: {{ status }}</p>
+          </div>
+        </div>
+        <div class="mainpage__header_userinfoitem">
+          <p class="mainpage__header_itemtext">Заданные треки</p>
+          <div class="mainpage__header_loading">
+            <p class="mainpage__header_loadinfo">{{ setTracks }}</p>
+          </div>
+        </div>
+        <div class="mainpage__header_userinfoitem">
+          <p class="mainpage__header_itemtext">Прохождение курса</p>
+          <div class="mainpage__header_loading">
+            <p
+              class="mainpage__header_loadinfo"
+              v-bind:class="[{ lightText: percentProgress >= 40 }]"
+            >
+              {{ percentProgress }}%
+            </p>
+            <div
+              class="mainpage__header_loadstatus"
+              :style="{ width: percentProgress + '%' }"
+            ></div>
+          </div>
+        </div>
+        <div class="mainpage__header_userinfoitem">
+          <p class="mainpage__header_itemtext">Пройденные треки</p>
+          <div class="mainpage__header_loading">
+            <p
+              class="mainpage__header_loadinfo"
+              v-bind:class="[{ lightText: tracksPercent >= 45 }]"
+            >
+              {{ finTracks }}/{{ allTracks }}
+            </p>
+            <div
+              class="mainpage__header_loadstatus"
+              :style="{ width: tracksPercent + '%' }"
+            ></div>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="mainpage__itemlist">
+      <div class="mainpage__itemlist_linkblock mainpage__itemlist_track" @click="toMyTracks">
+        <h3 class="mainpage__itemlist_headtext">Мои Треки</h3>
+        <div>
+          <p class="mainpage__itemlist_text">Перейти</p>
+          <p class="mainpage__itemlist_icon"><i class="far fa-play-circle"></i></p>
+        </div>
+      </div>
+      <div class="mainpage__itemlist_linkblock mainpage__itemlist_catalog" @click="toCatalog">
+        <h3 class="mainpage__itemlist_headtext">Каталог</h3>
+        <div>
+          <p class="mainpage__itemlist_text">Перейти</p>
+          <p class="mainpage__itemlist_icon"><i class="far fa-play-circle"></i></p>
+        </div>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
+
+
 export default {
-  name: "Main"
-}
+  name: "Main",
+  data() {
+    return {
+      userName: "Зигмунд Фрейд",
+      status: "Студент",
+      percentProgress: 39,
+      allTracks: 30,
+      finTracks: 15,
+      setTracks: 5,
+    };
+  },
+  computed: {
+    tracksPercent() {
+      return +(
+        100 -
+        ((this.allTracks - this.finTracks) / this.allTracks) * 100
+      ).toFixed(0);
+    },
+  },
+  methods: {
+    toCatalog() {
+      this.$router.push({path: "/catalogue"})
+    },
+    toMyTracks() {
+      this.$router.push({path: "/tracks"})
+    }
+  },
+};
 </script>
 
-<style scoped>
+<style lang="sass" scoped>
+.mainpage
+  margin: 0 auto
+  max-width: 1024px
+  &__header
+    display: flex
+    justify-content: space-between
+    padding-top: 24px
+    margin-left: 20px
+    margin-right: 20px
+    &_welcome
+      width: 480px
+      margin-right: 24px
+    &_headtext
+      font-size: 46px
+      font-weight: 400
+      line-height: 62px
+      text-align: left
+    &_maintext
+      font-weight: 400
+      font-size: 16px
+      text-align: left
+    &_user
+      max-width: 540px
+      display: grid
+      grid-gap: 8px
+      grid-template-columns: repeat(2, 1fr)
+      background-color: #ffffff
+      border-radius: 20px
+      box-sizing: border-box
+    &_itemtext
+      align-self: flex-start
+      font-weight: 100
+      font-size: 15px
+      color: #355E66
+
+    &_userinfo
+      border-radius: 25px
+      margin-top: 12px
+      margin-left: 12px
+      display: flex
+      // background-color: #355e6621
+    &_useravatar
+      width: 95px
+    &_userparams
+      margin-left: 16px
+      display: flex
+      flex-direction: column
+      align-items: flex-start
+      justify-content: center
+    &_userparamsname
+      font-size: 24px
+      font-weight: 400
+      margin-bottom: 16px
+    &_userparamsstatus
+      font-size: 16px
+      font-weight: 200
+
+    &_userinfoitem
+      display: flex
+      flex-direction: column
+      margin: 12px
+
+    &_loading
+      position: relative
+      width: 160px
+      height: 40px
+      border: 3px solid #355E66
+      border-radius: 50px
+      margin-top: 12px
+      display: flex
+      justify-content: center
+      align-items: center
+      flex-direction: column
+    &_loadstatus
+      height: 90%
+      background-color: #355E66
+      border-radius: 50px
+      color: #ffffff
+      font-size: 16px
+      margin-left: 2px
+      margin-right: 2px
+      align-self: flex-start
+    &_loadinfo
+      position: absolute
+      color: #355E66
+      text-shadow: 0 0 5px #ffffff
+  &__itemlist
+    display: grid
+    grid-template-columns: repeat(auto-fit, minmax(280px, 480px))
+    grid-gap: 24px
+    margin-top: 72px
+    justify-content: space-between
+    box-sizing: border-box
+    overflow: scroll
+    height: 640px
+    &_linkblock
+      position: relative
+      height: 280px
+      border-radius: 50px
+      margin-top: 20px
+      margin-left: 20px
+      margin-right: 20px
+      color: #ffffff
+      display: flex
+      flex-direction: column
+      justify-content: center
+      align-items: center
+      transition: all 0.2s
+      &:hover
+        box-shadow: 0 0 15px -5px black
+        transform: scale(1.01)
+      &:active
+        box-shadow: 0 0 8px -3px black
+        transform: scale(1)
+    &_headtext
+      position: absolute    
+      left: 24px
+      top: 34px
+      font-size: 24px
+      font-weight: 200
+    &_text
+      font-size: 16px
+      font-weight: 200
+    &_icon
+      font-size: 80px
+      margin-top: 8px
+    &_track
+      background-image: url(../assets/homeitem1.png)
+      background-size: cover
+    &_catalog
+      background-image: url(../assets/homeitem2.png)
+      background-size: cover
+.lightText
+  color: #ffffff
+  text-shadow: 0 0 8px #000000
+
 
 </style>
